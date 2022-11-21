c823e37c2a5a22a9248fd3c74fafa64cafb21623
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-21 15:05:56 +0100

    Adding Form Validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,16 +1,63 @@
+import { useState } from "react";
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [nameIsTouched, setNameIstouched] = useState(false);
+
+  const nameIsValid = enteredName.trim() !== "";
+  const nameIsInvalid = !enteredName && nameIsTouched;
+
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [emailIsTouched, setEmailIstouched] = useState(false);
+
+  const emailIsValid = enteredEmail.trim() !== "";
+  const emailIsInvalid = !enteredEmail && emailIsTouched;
+
+  let formIsValid = false;
+  if (nameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const nameChangeHander = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const emailChangeHander = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setEmailIstouched(true);
+    setNameIstouched(true);
+    if (!nameIsValid && !emailIsValid) {
+      return;
+    }
+  };
+  const nameFormClass = nameIsInvalid ? "form-control invalid" : "form-control";
+  const emailFormClass = emailIsInvalid
+    ? "form-control invalid"
+    : "form-control";
   return (
-    <form>
-      <div className="form-control">
+    <form onSubmit={submitHandler}>
+      <div className={nameFormClass}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameChangeHander}
+        />
       </div>
-      <div className="form-control">
+      <div className={emailFormClass}>
         <label htmlFor="email">Your Email</label>
-        <input type="email" id="email" />
+        <input
+          type="email"
+          id="email"
+          value={enteredEmail}
+          onChange={emailChangeHander}
+        />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
