919cd9d26047a3cf4ae9e6853928b3975f4d398c
“Hayaa”Qutaishat” <hayaa.qutaishat@gmail.com>
2022-11-21 14:55:07 +0100

    Simple Form Setup

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,19 @@
+const SimpleInput = (props) => {
+  return (
+    <form>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">Your Email</label>
+        <input type="email" id="email" />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
